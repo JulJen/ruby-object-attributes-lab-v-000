6ef13cb6d07bab65e8708104dd4bf5bb36c830af
Julia J-Prog <juliajen.prog@gmail.com>
2017-11-08 23:14:08 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,75 @@
+
+class Dog
+
+  def initialize(name, breed) # Hook
+    @name = name
+    @breed = breed
+
+    
+    # @breed = breed
+  end
+
+  # define setter method for name
+  def name=(new_dog_name) # Writer
+    @name = new_dog_name # Hoisting /casting local variable to an instance variable
+  end
+
+  # define getter method for name
+  def name # Reader
+    @name
+    # "#{@name}".strip
+  end
+end
+
+# fido = Dog.new("Fido")
+# fido.name
+#   def breed=(new_breed) # Writer
+#     @breed = new_breed # Hoisting
+#   end
+#
+#   def breed #Reader
+#     @breed
+#   end
+# end
+
+# fido = Dog.new
+# fido.breed
+#
+# fido.name = "Fido"
+# fido.name
+
+# fido = Dog.new("Fido")
+# fido.name
+# end
+
+ # define setter method for name
+
+
+
+   # => "Fido"
+
+
+
+  # context 'with names' do
+  #   describe '#name=' do
+  #     it 'writes the name of the dog to an instance variable @name' do
+  #       fido = Dog.new
+  #       fido.name = "Fido"
+  #
+  #       expect(fido.instance_variable_get(:@name)).to eq("Fido")
+  #     end
+  #   end
+
+#   def initialize(dogs_breed)
+#     @dogs_breed = dogs_breed
+#   end
+#
+#   def breed=(dogs_breed) # setter
+#     @dogs_breed = dogs_breed
+#   end
+#
+#   def breed # getter
+#     "#{@dogs_breed}".strip
+#   end
+#
+# end
