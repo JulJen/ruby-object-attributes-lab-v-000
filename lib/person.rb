644c56781bfef47c990db4b4c03608ca9b273315
Julia J-Prog <juliajen.prog@gmail.com>
2017-11-08 23:34:05 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,25 +1,20 @@
-# 
-# class Person
-#
-#   def initialize(name, job)
-#     @name = name
-#     @job = job
-#   end
-#
-#   def name=(name)
-#     @name = name
-#   end
-#
-#   def name
-#     "#{@name}".strip
-#   end
-#
-#   def job=(job)
-#     @job =  job
-#   end
-#
-#   def job
-#     "#{@job}".strip
-#   end
-#
-# end
+
+class Person
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job_type)
+    @job = joby_type
+  end
+
+  def job
+    @job
+  end
+
+end
